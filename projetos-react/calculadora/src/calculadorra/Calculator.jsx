dc36b4a567c383b93e67e98dca8ddd7d6ff3b9b3
thamyris <thamyriscabral@hotmail.com>
2023-01-14 12:16:38 -0300

    alteracoes

--- projetos-react/calculadora/src/calculadorra/Calculator.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Calculadora extends Component {
-  render() {
-    return <div className='calculadora'></div>;
-  }
-}
-
-export default Calculadora;
