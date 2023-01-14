7c48c6661f8f80cdc3001da2d3e1a181d9c7c115
thamyris <thamyriscabral@hotmail.com>
2023-01-14 12:01:05 -0300

    iniciando projeto calculadora com react

---
+++ projetos-react/calculadora/src/calculadorra/Calculator.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Calculadora extends Component {
+  render() {
+    return <div className='calculadora'></div>;
+  }
+}
+
+export default Calculadora;
