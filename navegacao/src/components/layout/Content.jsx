6b63932f094bc64c05efe21d74f5adecd787d6b9
thamyris <thamyriscabral@hotmail.com>
2023-02-01 16:26:50 -0300

    alterado os arquivos

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import './Content.css';
 
-import { Routes, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import About from '../../views/examples/About';
+import Home from '../../views/examples/Home';
 
 const Content = (props) => (
   <main className='Content'>
-    <Routes>
-      {/* <Route
-        path='/about'
-        element={<About />}
-      />
-      <Route
-        path='/param/:id'
-        element={<Param />}
-      />
-      <Route
-        path='/'
-        exact
-        element={<Home />}
-      />
-      <Route
-        path='*'
-        element={<NotFound />}
-      /> */}
-    </Routes>
+    <Switch>
+      <Route path='/about'>
+        <About></About>
+      </Route>
+      <Route path='/'>
+        <Home></Home>
+      </Route>
+    </Switch>
   </main>
 );
 
