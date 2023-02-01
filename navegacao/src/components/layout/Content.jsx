22c34101a67fff1b37b311dad9cc7e960b0855bc
thamyris <thamyriscabral@hotmail.com>
2023-02-01 16:04:40 -0300

    alterado os arquivos

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import './Content.css';
 
-const Content = (props) => <main className='Content'>Content</main>;
+import { Routes, Route } from 'react-router-dom';
+
+const Content = (props) => (
+  <main className='Content'>
+    <Routes>
+      {/* <Route
+        path='/about'
+        element={<About />}
+      />
+      <Route
+        path='/param/:id'
+        element={<Param />}
+      />
+      <Route
+        path='/'
+        exact
+        element={<Home />}
+      />
+      <Route
+        path='*'
+        element={<NotFound />}
+      /> */}
+    </Routes>
+  </main>
+);
 
 export default Content;
