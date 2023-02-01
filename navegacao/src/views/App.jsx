33d4f77e96164c9e2d55181554aca746259dd4a2
thamyris <thamyriscabral@hotmail.com>
2023-02-01 15:41:19 -0300

    criada as pastas

---
+++ navegacao/src/views/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Menu from '../components/layout/Menu';
+import Content from '../components/layout/Content';
+
+const App = (props) => (
+  <div className='App'>
+    <Menu></Menu>
+    <Content />
+  </div>
+);
+
+export default App;
